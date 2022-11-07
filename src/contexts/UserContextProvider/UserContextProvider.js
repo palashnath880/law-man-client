da8bb80e8083797758b9bb9498aef5f1d48079b4
Palash <palashnath880@gmail.com>
2022-11-08 00:17:53 +0600

    Firebase Init

--- src/contexts/UserContextProvider/UserContextProvider.js
+++ src/contexts/UserContextProvider/UserContextProvider.js
@@ -1,10 +1,12 @@
 import React, { createContext } from 'react';
+import { getAuth } from 'firebase/auth';
+import app from '../../firebase/__firebase.config';
 
 export const UserContext = createContext();
 
 const UserContextProvider = ({ children }) => {
 
-
+    const auth = getAuth(app);
 
     const userInfo = {};
 
